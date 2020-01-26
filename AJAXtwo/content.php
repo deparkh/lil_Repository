3c2b36ce81b66eb5a4dd34cd138429e0741f2a2a
pdanio <58370515+pdanio@users.noreply.github.com>
2020-01-26 02:54:32 +0200

    added AJAX drafts

---
+++ AJAXtwo/content.php
@@ -0,0 +1,4 @@
+<?
+  sleep(2);
+  echo "Information: name - ".$_POST['name'].", string - ".$_POST['stringi'].", number - ".$_POST['number'];
+?>
---
+++ AJAXtwo/countryCheck.php
@@ -0,0 +1,6 @@
+<?
+  if ($_GET["country"]==1) echo json_encode(array("1"=>
+  "Washington", "2"=>"Seattle"));
+  else if ($_GET["country"]==2) echo json_encode(array("3"=>
+  "Paris", "4"=>"Lyon"));
+?>
